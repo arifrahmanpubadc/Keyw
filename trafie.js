0d7a8e0dbaf9e0e2e50a0bd76d1b0acec96b113e
arifrahmanpubadc <122809812+arifrahmanpubadc@users.noreply.github.com>
2025-12-06 16:15:22 +0700

    Update trafie.js

--- trafie.js
+++ trafie.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://bhbajce.clicksmatching.com/s/63ef87ee2912d?track=Nxt&ext_click_id=Nxt"; } LoadAd();
+function LoadAd() { parent.location.href="https://oggoel.amazedatlngs.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=181648&s2=2193156&s5=Brozz&click_id=Brozz"; } LoadAd();
